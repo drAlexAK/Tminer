d7880e045e4c8adaf262ff49bb3e7ea024ffeea6
drAlexAK <alex.a.kudryashov@gmail.com>
2022-05-06 02:45:24 +0300

    bug fix

--- Console.h
+++ Console.h
@@ -6,11 +6,13 @@ class Console{
 public:
     static int Init();
     static int Restore();
+
     static void PrintSymbol(int x, int y, std::string s, Font f);
     static void PrintString(std::string s, Font f);
     static void Flush();
     static void Clear();
+    static void SetCursor(bool enable);
     static void Out(const std::string &s);
     static int In();
     static void Pause();
-};;
+};
