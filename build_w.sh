2fde2bb1c4d56c86db4e275150116e85fe9199fd
drAlexAK <alex.a.kudryashov@gmail.com>
2022-05-05 22:30:28 +0300

    add build file for Windows

---
+++ build_w.sh
@@ -0,0 +1,1 @@
+i686-w64-mingw32-g++ -o tMiner ./main.cpp ./Game.cpp ./Game.h ./Print.cpp ./Console.cpp ./Font.cpp ./Definitions.cpp ./Controller.cpp ./KeyboardW.cpp -static-libgcc -static-libstdc++
