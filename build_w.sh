c2db32fbaf347e48ca455736df7eb853c125f9d7
drAlexAK <alex.a.kudryashov@gmail.com>
2022-05-06 02:42:34 +0300

    bug fixed

--- build_w.sh
+++ build_w.sh
@@ -1,1 +1,1 @@
-i686-w64-mingw32-g++ -o tMiner ./main.cpp ./Game.cpp ./Game.h ./Print.cpp ./Console.cpp ./Font.cpp ./Definitions.cpp ./Controller.cpp ./KeyboardW.cpp -static-libgcc -static-libstdc++
+i686-w64-mingw32-g++ -o tMiner ./main.cpp ./Game.cpp ./Print.cpp ./ConsoleW.cpp ./Font.cpp ./Controller.cpp ./KeyboardW.cpp -static-libgcc -static-libstdc++
