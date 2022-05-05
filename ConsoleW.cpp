1fa591d8e2e020f04567159cef8c527df4c5175a
drAlexAK <alex.a.kudryashov@gmail.com>
2022-05-06 02:45:43 +0300

    Update ConsoleW.cpp

--- ConsoleW.cpp
+++ ConsoleW.cpp
@@ -1,11 +1,13 @@
-#include "ConsoleW.h"
+#include "Console.h"
+#include <windows.h>
 #include <iostream>
-
 
 static DWORD originalConsoleMode;;
 static HANDLE stdoutHandle;
 
 int Console::Init() {
+
+    SetConsoleOutputCP(CP_UTF8);
 
     DWORD currentConsoleMode = 0;
 
@@ -26,8 +28,6 @@ int Console::Init() {
     if(!SetConsoleMode(stdoutHandle, currentConsoleMode)) {
         return(GetLastError());
     }
-
-    SetConsoleOutputCP(CP_UTF8);
 }
 
 int Console::Restore() {
@@ -43,6 +43,15 @@ int Console::Restore() {
 void Console::Pause(){
     system("pause");
 }
+
+void Console::SetCursor(bool enable){
+    static const HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_CURSOR_INFO cci;
+    GetConsoleCursorInfo(handle, &cci);
+    cci.bVisible = enable; // show/hide cursor
+    SetConsoleCursorInfo(handle, &cci);
+}
+
 void Console::Flush(){
     std::fflush(stdout);
 }
@@ -50,5 +59,49 @@ void Console::Flush(){
 void Console::PrintSymbol(int x, int y, const std::string s, Font f) {
     std::string cmd = f.ToString();
     printf("\033[%d;%dH\x1b[%sm%s\x1b[0m", x+1, y+1, cmd.c_str(), s.c_str());
+    Console::Flush();
+}
+
+void Console::PrintString(const std::string s, Font f){
+    std::string cmd = f.ToString();
+    for(int i = 0; i < (int)s.size(); i++){
+        std::string ch = s.substr(i, 1);
+        printf("\x1b[%sm%s\x1b[0m", cmd.c_str(), ch.c_str());
+    }
     Flush();
+}
+
+void Console::Clear(){
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    DWORD count;
+    DWORD cellCount;
+
+    COORD homeCoords = { 0, 0 };
+
+    stdoutHandle = GetStdHandle( STD_OUTPUT_HANDLE );
+    if (stdoutHandle == INVALID_HANDLE_VALUE) return;
+
+    // Get the number of cells in the current buffer
+    if (!GetConsoleScreenBufferInfo( stdoutHandle, &csbi )) return;
+    cellCount = csbi.dwSize.X *csbi.dwSize.Y;
+
+    // Fill the entire buffer with spaces
+    if (!FillConsoleOutputCharacter(stdoutHandle, (TCHAR) ' ', cellCount, homeCoords, &count)) return;
+
+    // Fill the entire buffer with the current colors and attributes
+    if (!FillConsoleOutputAttribute( stdoutHandle, csbi.wAttributes, cellCount, homeCoords, &count)) return;
+
+    // Move the cursor top-left corner 
+    SetConsoleCursorPosition( stdoutHandle, homeCoords );
+
+}
+
+void Console::Out(const std::string &s){
+    std::cout << s;
+}
+
+int Console::In(){
+    int a;
+    std::cin >> a;
+    return a;
 }
